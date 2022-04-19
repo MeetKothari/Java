2c7acc58909edbd37f9f5f28f0603f342fcfb595
Meet Kothari <72781025+MeetKothari@users.noreply.github.com>
2022-04-19 15:10:15 -0400

    Create module2.java

---
+++ Code Academy/module2.java
@@ -0,0 +1,11 @@
+public class MyProfile {
+	public static void main(String[] args) { 
+
+    String name = "Meet";
+    int age = 20;
+    double desiredSalary = 1000000.00;
+    char gender = 'M';  
+    boolean lookingForJob = true;
+
+	}
+}
